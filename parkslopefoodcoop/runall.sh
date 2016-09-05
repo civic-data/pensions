9ca55d776d57cf8aab3c522ae9e0bd579c3b8ee2
Ralph Yozzo <fedex1@gmail.com>
2016-09-04 22:00:55 -0400

    add plan number to file, what a mess the Department of Labor data is\!

--- parkslopefoodcoop/runall.sh
+++ parkslopefoodcoop/runall.sh
@@ -1,2 +1,21 @@
 #!
-grep "Total plan assets" *.txt.clean |awk '{print $1 "," $(NF-1) "," $NF }'
+# grep "Total plan assets" *.txt.clean |awk '{print $1 "," $(NF-1) "," $NF }'
+
+
+cp    2009_636094.txt.clean                                   plan_number-1-Year-2009_636094.data.txt
+cp    2009_696988.txt.clean                                   plan_number-1-Year-2009_696988.data.txt
+cp    2010_1313836.txt.clean                                  plan_number-1-Year-2010_1313836.data.txt
+cp    2011_2265606.txt.clean                                  plan_number-1-Year-2011_2265606.data.txt
+cp    2011_2269856.txt.clean                                  plan_number-1-Year-2011_2269856.data.txt
+cp    2012_3262236.txt.clean                                  plan_number-1-Year-2012_3262236.data.txt
+cp    2013_4042293.txt.clean                                  plan_number-1-Year-2013_4042293.data.txt
+cp    2013_4045156.txt.clean                                  plan_number-1-Year-2013_4045156.data.txt
+cp    2014_4853295.txt.clean                                  plan_number-1-Year-2014_4853295.data.txt
+cp    2015_5758026.txt.clean                                  plan_number-1-Year-2015_5758026.data.txt
+cp    2009_607381.txt.clean                                   plan_number-2-Year-2009_607381.data.txt
+cp    2010_1453548.txt.clean                                  plan_number-2-Year-2010_1453548.data.txt
+cp    2011_2607786.txt.clean                                  plan_number-2-Year-2011_2607786.data.txt
+cp    2012_3126031.txt.clean                                  plan_number-2-Year-2012_3126031.data.txt
+cp    2013_4279173.txt.clean                                  plan_number-2-Year-2013_4279173.data.txt
+cp    2014_5100967.txt.clean                                  plan_number-2-Year-2014_5100967.data.txt
+cp    2015_5986220.txt.clean                                  plan_number-2-Year-2015_5986220.data.txt
